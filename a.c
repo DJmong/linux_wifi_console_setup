6086da54dc68eadce92852b5fbf42c996d9ed039
DJmong <49215689+DJmong@users.noreply.github.com>
2019-07-16 14:43:23 +0900

    Add files via upload

---
+++ a.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define false 0
+static int count(char n[]){
+	int i;
+	while(1){
+		i++;
+		if(n[i]=='\0') return i;
+	}
+}
+
+void main(){
+	_Bool wpa_true;
+	char id[30];
+	char passwd[30];
+	char wpa[130];
+	do{
+	printf("enter your wifi-id : ");
+	scanf("%s", id);
+
+	printf("enter your wifi-password : ");
+	scanf("%s", passwd);
+
+
+	strcpy(wpa, "wpa_passphrase ");
+
+	strcat(wpa,id);
+	strcat(wpa," ");
+	strcat(wpa,passwd);
+
+	}while(system(wpa));
+
+	strcat(wpa, " |sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf");
+
+	wpa_true=system(wpa);
+	if(wpa_true==0) printf("error, check your permission");
+	else printf("wifi setup is completed");
+
+}
+
+
